9121d6dd7f2df4588be39e8a52bfafc9a0ed1358
Elvin Rivera <elvinkrivera@gmail.com>
2021-08-26 13:36:36 -0700

    - Day 38 Step 1 - setup API credentials and google spreadsheet

--- intermediatePLUS_programs/day38-WorkoutTracking-GoogleSheets/main.py
+++ intermediatePLUS_programs/day38-WorkoutTracking-GoogleSheets/main.py
@@ -4,4 +4,11 @@
 # Description: Workout Tracking Using Google Sheets
 #
 # References: https://beta.openai.com/docs/introduction/overview ,
-# https://www.nutritionix.com/business/api , https://sheety.co
+# https://www.nutritionix.com/business/api , https://sheety.co ,
+# Nutritionix API v2 Doc https://docs.google.com/document/d/1_q-K-ObMTZvO0qUEAxROrN3bwMujwAN25sLHwJzliK0/edit#
+import os
+
+import requests
+
+API_KEY = os.environ["Nutritionix API Key"]
+APP_ID = os.environ["Nutritionix App ID"]

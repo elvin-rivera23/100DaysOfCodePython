cca9dffd66a9f6dceadd59407361a1773c043b61
Elvin Rivera <elvinkrivera@gmail.com>
2021-08-26 09:28:54 -0700

    - Day 38 main.py created, added project description, and add reference links

---
+++ intermediatePLUS_programs/day38-WorkoutTracking-GoogleSheets/main.py
@@ -0,0 +1,7 @@
+# Elvin Rivera
+# 8/26/21
+#
+# Description: Workout Tracking Using Google Sheets
+#
+# References: https://beta.openai.com/docs/introduction/overview ,
+# https://www.nutritionix.com/business/api , https://sheety.co

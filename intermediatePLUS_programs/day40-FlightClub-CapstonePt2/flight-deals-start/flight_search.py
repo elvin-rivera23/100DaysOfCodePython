210c426255cc482a661a0922ef3415a0a77e179f
Elvin Rivera <elvinkrivera@gmail.com>
2021-08-30 10:05:06 -0700

    day40-FlightClub: update main.py, flight_search.py. Exception Handling for Destinations WITHOUT flights.

--- intermediatePLUS_programs/day40-FlightClub-CapstonePt2/flight-deals-start/flight_search.py
+++ intermediatePLUS_programs/day40-FlightClub-CapstonePt2/flight-deals-start/flight_search.py
@@ -4,22 +4,28 @@
 
 import requests
 from flight_data import FlightData
+import os
+from pprint import pprint
 
 TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
 TEQUILA_API_KEY = "My_API_KEY"  # hidden when uploading to github
 
 class FlightSearch:
+    def __init__(self):
+        self.city_codes = []
 
     # pass in city name to get destination code
-    def get_destination_code(self, city_name):
+    def get_destination_code(self, city_names):
         location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
         headers = {"apikey": TEQUILA_API_KEY}
-        query = {"term": city_name, "location_types": "city"}
-        response = requests.get(url=location_endpoint, headers=headers, params=query)   # api request for tequila query
-        results = response.json()["locations"]
-        # code = "Testing for now"    # just putting this here for now to get Sheety to work
-        code = results[0]["code"]  # first element
-        return code
+        for city in city_names:
+            query = {"term": city_names, "location_types": "city"}
+            response = requests.get(url=location_endpoint, headers=headers, params=query)   # api request for tequila query
+            results = response.json()["locations"]
+            # code = "Testing for now"    # just putting this here for now to get Sheety to work
+            code = results[0]["code"]  # first element
+            self.city_codes.append(code)
+        return self.city_codes
 
     # The next step is to search for the flight prices from London (LON) to all the destinations in the Google Sheet.
     # In this project, we're looking only for direct flights, that leave anytime between tomorrow and in 6 months (
@@ -48,18 +54,19 @@ class FlightSearch:
         # if destination exists,that becomes new data, if it reaches index error exit
         try:
             data = response.json()["data"][0]
+            print(f"{destination_city_code} {data['price']}")
         except IndexError:
             print(f"No flights found for {destination_city_code}.")
             return None
-
-        flight_data = FlightData(
-            price=data["price"],
-            origin_city=data["route"][0]["cityFrom"],
-            origin_airport=data["route"][0]["flyFrom"],
-            destination_city=data["route"][0]["cityTo"],
-            destination_airport=data["route"][0]["flyTo"],
-            out_date=data["route"][0]["local_departure"].split("T")[0],
-            return_date=data["route"][0]["local_departure"].split("T")[0],
-        )
-        print(f"{flight_data.destination_city}: ${flight_data.price}")
-        return  flight_data
+        else:
+            flight_data = FlightData(
+                price=data["price"],
+                origin_city=data["route"][0]["cityFrom"],
+                origin_airport=data["route"][0]["flyFrom"],
+                destination_city=data["route"][0]["cityTo"],
+                destination_airport=data["route"][0]["flyTo"],
+                out_date=data["route"][0]["local_departure"].split("T")[0],
+                return_date=data["route"][0]["local_departure"].split("T")[0],
+            )
+            # print(f"{flight_data.destination_city}: ${flight_data.price}")
+            return flight_data
--- intermediatePLUS_programs/day40-FlightClub-CapstonePt2/flight-deals-start/main.py
+++ intermediatePLUS_programs/day40-FlightClub-CapstonePt2/flight-deals-start/main.py
@@ -32,27 +32,50 @@ ORIGIN_CITY_IATA = "LON"
 # For now, the FlightSearch class can respond with "TESTING" instead of a real IATA code. You should use the response
 # from the FlightSearch class to update the sheet_data dictionary.
 
+# if sheet_data[0]["iataCode"] == "":
+#     # sheet data is in dictionary/json format
+#     for row in sheet_data:
+#         row["iataCode"] = flight_search.get_destination_code(row["city"])
+#     # print(f"sheet_data:\n {sheet_data}")
+#     # print(sheet_data)
+#     data_manager.destination_data = sheet_data
+#     data_manager.update_destination_codes()
+
 if sheet_data[0]["iataCode"] == "":
-    # sheet data is in dictionary/json format
-    for row in sheet_data:
-        row["iataCode"] = flight_search.get_destination_code(row["city"])
-    # print(f"sheet_data:\n {sheet_data}")
-    # print(sheet_data)
-    data_manager.destination_data = sheet_data
+    city_names = [row["city"] for row in sheet_data]   # list comprehension
+    data_manager.city_codes = flight_search.get_destination_code(city_names)
     data_manager.update_destination_codes()
+    sheet_data = data_manager.get_destination_data()
+
+# dictionary comprehension
+destinations = {
+    data["iataCode"]: {
+        "id": data["id"],
+        "city": data["city"],
+        "price": data["lowestPrice"],
+    } for data in sheet_data}
+
 
 tomorrow = datetime.now() + timedelta(days=1)
 six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
 
-for destination in sheet_data:
+for destination_code in destinations:
     flight = flight_search.check_flights(
         ORIGIN_CITY_IATA,
-        destination["iataCode"],
         from_time=tomorrow,
         to_time=six_month_from_today,
     )
 
-    if flight.price < destination["lowestPrice"]:
-        notification_manager.send_sms(
-            message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}. "
-        )
+    if flight is None:
+        continue
+
+    if flight.price < destinations[destination_code]["price"]:
+        users = data_manager.get_customer_emails()
+        emails = [row["email"] for row in users]
+        names = [row["firstName"] for row in users]
+        message = f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}. "
+        if flight.stop_overs > 0:
+            message += f"\n\nFlight has {flight.stop_overs}, via {flight.via_city}."
+        link = f"https://www.google.co.uk/flights?hl=en#flt={flight.origin_airport}.{flight.destination_airport}.{flight.out_date}*{flight.destination_airport}.{flight.origin_airport}.{flight.return_date}"
+
+        notification_manager.send_sms(emails, message, link)

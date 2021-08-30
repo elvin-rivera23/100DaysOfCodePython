7e50993ae7e7ffcda8661753a9d6157fa1870bc7
Elvin Rivera <elvinkrivera@gmail.com>
2021-08-29 17:08:42 -0700

    - day 39: update data manager class, get destination data and update that data with IATA codes.

--- intermediatePLUS_programs/day39-FlightDealFinder-Capstone/flight-deals-start/data_manager.py
+++ intermediatePLUS_programs/day39-FlightDealFinder-Capstone/flight-deals-start/data_manager.py
@@ -1,3 +1,30 @@
+import requests
+
+SHEETY_PRICES_ENDPOINT = "SHEETY PRICES ENDPOINT URL"
+
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.destination_data = {}      # destination data is dictionary format
+
+    def get_destination_data(self):
+        # 2. use the Sheety API to GET all the data in that sheet and print it out.
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        data = response.json()
+        self.destination_data = data["prices"]  # destination data from json, prices is key
+        return self.destination_data
+
+    def update_destination_data(self):
+        for city in self.destination_data:
+            # update destination by creating new dict, price is key, value is dictionary of iataCodes
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
--- intermediatePLUS_programs/day39-FlightDealFinder-Capstone/flight-deals-start/main.py
+++ intermediatePLUS_programs/day39-FlightDealFinder-Capstone/flight-deals-start/main.py
@@ -1,1 +1,10 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+# Elvin Rivera
+# 8/29/21
+#
+# Description: Appliction for finding flight deals. Capstone project: OOP, APIs, datetime, List and Dictionary Comprehensions
+
+# This file will need to use the DataManager,FlightSearch, FlightData,
+# NotificationManager classes to achieve the program requirements.
+
+# Resources: https://sheety.co , https://partners.kiwi.com , https://tequila.kiwi.com/portal/login ,
+# https://www.twilio.com/docs/sms
